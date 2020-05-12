621ae96961d82bfac9017d3278b3b7aeac7ec878
Evgeny Svirsky <evgeny@cere.io>
2020-05-12 21:20:47 +0300

    * Added routing.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import {
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+
 import ArticleList from "./containers/ArticleList";
+import ArticleEdit from "./components/ArticleEdit";
 
 function App() {
+  function renderNavigation() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Medium</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className='nav-link' activeClassName='active' to="/" exact>Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className='nav-link' activeClassName='active' to="/articles">Articles</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className='nav-link' activeClassName='active' to="/create-article">Create Article</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+
   return (
     <div className="container">
-      <h1>Medium</h1>
-      <ArticleList />
+      {renderNavigation()}
+      <Switch>
+        <Route path="/" exact>
+          <h3>Welcome to Medium!</h3>
+        </Route>
+        <Route path="/articles">
+          <ArticleList />
+        </Route>
+        <Route path="/create-article">
+          <ArticleEdit />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/ArticleEdit.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ArticleEdit() {
+  return (
+    <div>Article Edit</div>
+  );
+}
+
+export default ArticleEdit;
